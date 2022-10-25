7d8935c4bdcde83f6579baf66b6be173c64dc979
kendricksharpe <kendricksharpe@berkeley.edu>
2022-10-25 15:20:37 -0400

    This commit sets up texting capabilities, not yet set up but you know the vibe

--- main.py
+++ main.py
@@ -5,9 +5,12 @@
 #####
 
 import random
+from twilio.rest import Client #You can comment this out if you don't have twilio installed
 
-TWILIO_SECRET = None #Replace ... with your twilio secret for sending texts
-PHONE_NUMBER = None #Replace ... with your phone number
+# Your Account SID from twilio.com/console
+account_sid = ""
+# Your Auth Token from twilio.com/console
+auth_token  = ""
 
 #Loads the QUOTES with data from quotes.txt
 #Returns array of quotes
@@ -23,7 +26,15 @@ def load_quote_array():
 def random_selection(quotes):
     return random.choice(quotes)
 
+def send_text(quote):
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        to="", #replace with target phone number
+        from_="", #replace with twilio number
+        body=quote)
 
 if __name__ == "__main__":
     QUOTES = load_quote_array()
-    print("-=+=-\n\n" + random_selection(QUOTES) + "\n\n-+-")
+    quote = random_selection(QUOTES)
+    print("-=+=-\n\n" + quote + "\n\n-+-\n")
+    send_text(quote)
